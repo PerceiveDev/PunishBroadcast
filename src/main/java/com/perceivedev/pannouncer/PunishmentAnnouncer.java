acfb141d848078824dfe04e4773355246d69db79
ZP4RKER <ZP4RKER@PROGRAMMER.NET>
2016-10-18 22:05:55 +1100

    Added default config, Added CommandListener

--- src/main/java/com/perceivedev/pannouncer/PunishmentAnnouncer.java
+++ src/main/java/com/perceivedev/pannouncer/PunishmentAnnouncer.java
@@ -3,5 +3,10 @@ package com.perceivedev.pannouncer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PunishmentAnnouncer extends JavaPlugin {
+	
+	public void onEnable() {
+		// Save default config
+		saveDefaultConfig();
+	}
 
 }
---
+++ src/main/java/com/perceivedev/pannouncer/lstnr/CommandListener.java
@@ -0,0 +1,14 @@
+package com.perceivedev.pannouncer.lstnr;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class CommandListener implements Listener {
+	
+	@EventHandler
+	public void onCommandEvent(PlayerCommandPreprocessEvent event) {
+		// Do stuff
+	}
+
+}
